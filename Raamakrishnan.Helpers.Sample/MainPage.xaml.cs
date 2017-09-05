8aed510944eb8c7c45f3bc5cdc25fdea5824d494
Raamakrishnan A <raamakrishnan1997@gmail.com>
2017-09-06 00:24:49 +0530

    Inital commit

---
+++ Raamakrishnan.Helpers.Sample/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Raamakrishnan.Helpers;
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Raamakrishnan.Helpers.Sample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+		private async void GoBtn_Click(object sender, RoutedEventArgs e)
+		{
+			LaunchLinkResult a = await AppLinks.LaunchLinkAsync(new Uri(AppUriTxt.Text, UriKind.Absolute), new Uri(WebUriTxt.Text, UriKind.Absolute));
+			StatusTxt.Text = $"Launcher: {a.LinkLaunchedBy}, IsLinkLaunched: {a.IsLinkLaunched}";
+		}
+	}
+}
---
+++ Raamakrishnan.Helpers/AppLinks.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.System;
+
+namespace Raamakrishnan.Helpers
+{
+    public class AppLinks
+    {
+		/// <summary>
+		/// Launch the URI scheme based AppUri if there is an app to handle, else open the browser
+		/// </summary>
+		/// <param name="AppUri">Based on the URI scheme of the app</param>
+		/// <param name="WebUri">Web URI</param>
+		/// <returns>Returns a LaunchLinkResult object</returns>
+		public async static Task<LaunchLinkResult> LaunchLinkAsync(Uri AppUri, Uri WebUri)
+		{
+			LaunchLinkResult result = new LaunchLinkResult() { IsLinkLaunched = false, LinkLaunchedBy = LinkLauncher.Unknown };
+			switch (await Launcher.QueryUriSupportAsync(AppUri, LaunchQuerySupportType.Uri))
+			{
+				case LaunchQuerySupportStatus.Available:
+					result.LinkLaunchedBy = LinkLauncher.App;
+					result.IsLinkLaunched = await Launcher.LaunchUriAsync(AppUri);
+					break;
+				default:
+					result.LinkLaunchedBy = LinkLauncher.WebBrowser;
+					result.IsLinkLaunched = await Launcher.LaunchUriAsync(WebUri);
+					break;
+			}
+			return result;
+		}
+
+		/// <summary>
+		/// Launch the URI scheme based AppUri if there is an app to handle, else open the browser
+		/// </summary>
+		/// <param name="AppUri">Based on the URI scheme of the app</param>
+		/// <param name="WebUri">Web URI</param>
+		/// <param name="Options">LaunchOptions that needs to be passed</param>
+		/// <returns>Returns a LaunchLinkResult object</returns>
+		public async static Task<LaunchLinkResult> LaunchLinkAsync(Uri AppUri, Uri WebUri, LauncherOptions Options)
+		{
+			LaunchLinkResult result = new LaunchLinkResult() { IsLinkLaunched = false, LinkLaunchedBy = LinkLauncher.Unknown };
+			switch (await Launcher.QueryUriSupportAsync(AppUri, LaunchQuerySupportType.Uri))
+			{
+				case LaunchQuerySupportStatus.Available:
+					result.LinkLaunchedBy = LinkLauncher.App;
+					result.IsLinkLaunched = await Launcher.LaunchUriAsync(AppUri, Options);
+					break;
+				default:
+					result.LinkLaunchedBy = LinkLauncher.WebBrowser;
+					result.IsLinkLaunched = await Launcher.LaunchUriAsync(WebUri, Options);
+					break;
+			}
+			return result;
+		}
+	}
+
+	public class LaunchLinkResult
+	{
+		public LinkLauncher LinkLaunchedBy;
+		public Boolean IsLinkLaunched;
+	}
+
+	/// <summary>
+	/// Tells what handled the link, the App or the WebBrowser. Returns Unknown if any error occurs.
+	/// </summary>
+	public enum LinkLauncher
+	{
+		App,
+		WebBrowser,
+		Unknown
+	}
+}
